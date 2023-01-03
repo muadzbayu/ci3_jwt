40f050167bd96bbc314bfc3cba03da9e064123dc
muadzbayu <muadz.bayual@gmail.com>
2023-01-03 12:46:21 +0700

    added api users:get and connect db

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-use chriskacerguis\RestServer\RestController;
+use chriskacerguis\RestServer\RestController;
 
 class Api extends RestController{
     function __construct(){
@@ -12,9 +12,31 @@ class Api extends RestController{
     public function users_get(){
         // Users from a data store e.g. database
         $users = [
-            ['id' => 0, 'name' => 'John', 'email' => 'john@example.com'],
-            ['id' => 1, 'name' => 'Jim', 'email' => 'jim@example.com'],
+            ['id' => 0, 'name' => 'bayu', 'email' => 'bayunutech@gmail.com'],
+            ['id' => 1, 'name' => 'aji', 'email' => 'ajinutech@gmail.com'],
         ];
+
+        $id = $this->get('id');
+
+        if($id === null){
+            if($users){
+                $this->response($users, 200);
+            }else{
+                $this->response([
+                    'status' => false,
+                    'message' => 'No users were found'
+                ], 404);
+            }
+        }else{
+            if(array_key_exists($id, $users)){
+                $this->response($users[$id], 200);
+            }else{
+                $this->response([
+                    'status' => false,
+                    'message' => 'No such user found'
+                ], 404);
+            }
+        }
         
     }
 }
